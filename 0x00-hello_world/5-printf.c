692b321d4ed4555b0d2bf6a7cf29dd7184de915a
sejo-moyez <moyajoseph439@gmail.com>
2022-09-08 22:12:01 +0300

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+*
+*main - prints out 
+*
+*Return: always 0
+*/
+
+int main(void)
+{
+	printf("with prwoper grammer, but the outcome is a piece of art\n");
+	return (0);
+}
