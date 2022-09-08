f5dcdc57883fb751214a7d096a6674c62b7f73fc
sejo-moyez <moyajoseph439@gmail.com>
2022-09-08 22:15:00 +0300

    hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 /**
 *
-*main - prints out 
+* main - prints out 
 *
-*Return: always 0
+* Return: always 0
 */
 
 int main(void)
 {
-	printf("with prwoper grammer, but the outcome is a piece of art\n");
+	printf("with proper grammer, but the outcome is a piece of art,\n");
 	return (0);
 }
