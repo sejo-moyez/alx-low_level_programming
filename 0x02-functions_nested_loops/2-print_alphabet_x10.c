80aca1515d71d0b9010cdcc89a2cd5f8ae607ef3
sejo-moyez <moyajoseph439@gmail.com>
2022-09-15 23:42:21 +0300

    prints alphabet 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+  * print_alphabet_x10 - prints the alphabet 10 times
+  *
+  */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char c;
+
+	while (i < 10)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+		i++;
+	}
+}
