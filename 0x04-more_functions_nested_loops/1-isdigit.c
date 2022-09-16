ba0e29a55c4f3fa50f01b21062214b3b6772ea69
sejo-moyez <107212756+sejo-moyez@users.noreply.github.com>
2022-09-16 23:27:32 +0300

    Update 1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -9,11 +9,11 @@
 int _isdigit(int x)
 {
 	if (x >= '0' && x <= '9')
-  {
-    if (x >= 48 && x <= 57)
-	  {
-	  	return (1);
-	  }
-  }
+	{
+		if (x >= 48 && x <= 57)
+		{
+			return (1);
+		}
+	}
 	return (0);
 }
