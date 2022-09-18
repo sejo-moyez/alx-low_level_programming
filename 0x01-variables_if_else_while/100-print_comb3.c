20e72848dd4904a88c05df8b01d36bda85a93f91
sejo-moyez <107212756+sejo-moyez@users.noreply.github.com>
2022-09-18 23:18:20 +0300

    Create 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+*main - prints all possible combinations of numbers
+*Return: always 0
+*/
+int main()
+{
+	int a, b;
+
+	for (a = 48; a <=56; a++)
+	{
+		for (b = 49;b <=57; b++)
+		
+		{
+		if (a == b)
+		continue;
+		putchar (a);
+		putchar (b);
+		putchar (',');
+		putchar (' ');
+		}
+
+	}
+	putchar ('\n');
+	return (0);
+}
