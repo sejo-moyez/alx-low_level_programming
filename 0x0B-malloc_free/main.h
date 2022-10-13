758f251d10f41b56aba50e2a366bcc7ba8a56ab8
sejo-moyez <107212756+sejo-moyez@users.noreply.github.com>
2022-10-13 17:34:33 +0300

    Update main.h

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,1 +1,14 @@
-char *create_array(unsigned int size, char c);
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+int _putchar(char);
+char *create_array(unsigned int, char);
+char *_strdup(char *);
+char *str_concat(char *, char *);
+int **alloc_grid(int, int);
+void free_grid(int **, int);
+char *argstostr(int, char **);
+
+#endif /* MAIN_H */
