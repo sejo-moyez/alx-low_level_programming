6df209be0563afb1a1370951fe30de07a7d289ac
sejo-moyez <107212756+sejo-moyez@users.noreply.github.com>
2022-12-14 17:18:46 +0300

    Update isupper.c

--- 0x18-dynamic_libraries/isupper.c
+++ 0x18-dynamic_libraries/isupper.c
@@ -1,1 +1,19 @@
-ii
+#include "main.h"
+/**
+*_isupper - determins if uppercase
+*
+*@c: parameter integer
+*
+*Return: 1 if uppercase else 0
+*/
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
