5a822db717027e5e714bc8279eb2628f2c4d6438
sejo-moyez <107212756+sejo-moyez@users.noreply.github.com>
2022-12-14 17:41:08 +0300

    Update strncat.c

--- 0x18-dynamic_libraries/strncat.c
+++ 0x18-dynamic_libraries/strncat.c
@@ -1,27 +1,30 @@
 #include "main.h"
+
 /**
-*_strcat - concatenates two strings
-*
-*@dest: string parameter
-*
-*@src: string parameter
-*
-*Return: concatenated string
+* _strncat - concatenate string
+* @dest: string to concatenate to
+* @src: string to concatenate
+* @n: max byte value
+* Return: (dest)
 */
-char *_strcat(char *dest, char *src)
+
+char *_strncat(char *dest, char *src, int n)
 {
-	int i, j;
-
-	for (j = 0; *(dest + j) != '\0'; j++)
-	{
-	}
-	for (i = 0; src[i] != '\0'; i++)
-	{
-		dest[j] = src[i];
-		j++;
-	}
-
-	dest[j] = '\0';
-
-	return (dest);
+int i, j, destlen, srclen;
+destlen = 0;
+srclen = 0;
+for (i = 0; dest[i] != '\0'; i++)
+{
+destlen++;
+}
+for (j = 0; src[j]; srclen++)
+{
+if (srclen < n)
+{
+dest[destlen] = src[srclen];
+}
+destlen++;
+j++;
+}
+return (dest);
 }
