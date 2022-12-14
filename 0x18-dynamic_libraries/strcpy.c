7442eb9d685e67c995fcde4269cd8126df679591
sejo-moyez <107212756+sejo-moyez@users.noreply.github.com>
2022-12-14 17:38:31 +0300

    Update strcpy.c

--- 0x18-dynamic_libraries/strcpy.c
+++ 0x18-dynamic_libraries/strcpy.c
@@ -1,25 +1,21 @@
 #include "main.h"
-/**
-*_strncpy - copies a string
-*
-*@dest: string parameter
-*@src: string parameter
-*@n: bytes to be copied
-*
-*Return: Dest
-*/
-char *_strncpy(char *dest, char *src, int n)
-{
-	int i;
 
-	for (i = 0; i < n && *(src + i) != '\0'; i++)
-	{
-		*(dest + i) = *(src + i);
-	}
-	while (i < n)
-	{
-		*(dest + i) = '\0';
-		i++;
-	}
-	return (dest);
+/**
+*_strcpy - copy string to buffer
+* @dest: destination
+* @src: string to copy
+* Return: pointer to buffer
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+int i, len;
+len = _strlen(src);
+*dest = len;
+for (i = 0; src[i] != '\0'; i++)
+{
+dest[i] = src[i];
+}
+dest[i] = '\0';
+return (dest);
 }
