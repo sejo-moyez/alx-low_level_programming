cc85402f117d20f95680c1aaa4b9775fed65709f
sejo-moyez <107212756+sejo-moyez@users.noreply.github.com>
2022-12-14 17:21:57 +0300

    Update puts.c

--- 0x18-dynamic_libraries/puts.c
+++ 0x18-dynamic_libraries/puts.c
@@ -1,1 +1,16 @@
-ii
+#include "main.h"
+/**
+*_puts - outputs string
+*
+*@str: pointer to string
+*/
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; *(str + i) != '\0'; i++)
+	{
+		_putchar(*(str + i));
+	}
+	_putchar('\n');
+}
