8273daab8fe2e2c052fb5b589bf400a8e74e74fb
sejo-moyez <107212756+sejo-moyez@users.noreply.github.com>
2022-12-14 17:17:38 +0300

    Update islower.c

--- 0x18-dynamic_libraries/islower.c
+++ 0x18-dynamic_libraries/islower.c
@@ -1,1 +1,19 @@
-ii
+#include "main.h"
+/**
+*_islower - Determines if character is lowercase
+*
+*@c: is parameter for a character
+*
+*Return: 1 (Success), 0 (Fail)
+*/
+int _islower(int c)
+{
+	if (c > 96 && c < 123)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
